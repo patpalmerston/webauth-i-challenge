db41b0ce8123ab3e8fdf010ce3e6ff622e5b8594
patpalmerston <patpalmerston@gmail.com>
2019-05-13 14:14:08 -0700

    post function up with Auth

--- data/users/users-router.js
+++ data/users/users-router.js
@@ -2,6 +2,35 @@ const router = require('express').Router();
 
 const db = require('./users-model');
 
+router.post('/register', (req, res) => {
+  let user = req.body;
+
+  db.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+})
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  db.findBy({ username })
+    .first()
+    .then(user => {
+      if (user) {
+        res.status(200).json({ message: `Welcome ${user.username}!`})
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' })
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+})
+
 router.get('/', (req, res) => {
   db.find()
     .then(users => {
