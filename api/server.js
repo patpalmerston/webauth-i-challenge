3dac5e6f5675e7f4e16a5d492d66eaa0475c7ca1
patpalmerston <patpalmerston@gmail.com>
2019-05-13 13:00:41 -0700

    initial instals and server up

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const configureMiddleware = require('../data/config/middleware');
+
+// const usersRouter = require('../data/users/users-router');
+
+const server = express();
+
+configureMiddleware(server)
+
+// server.use('/api/users', usersRouter)
+
+server.get('/', (req, res, next) => {
+  res.send(`<h2>We are LIVE!<h2>`)
+})
+
+module.exports = server
