a26643eb7e780a1cc68659a1fd0102e701a60a85
patpalmerston <patpalmerston@gmail.com>
2019-05-13 13:53:49 -0700

    get functional and tables up on to post

--- api/server.js
+++ api/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const configureMiddleware = require('../data/config/middleware');
 
-// const usersRouter = require('../data/users/users-router');
+const usersRouter = require('../data/users/users-router');
 
 const server = express();
 
 configureMiddleware(server)
 
-// server.use('/api/users', usersRouter)
+server.use('/api/users', usersRouter)
 
 server.get('/', (req, res, next) => {
   res.send(`<h2>We are LIVE!<h2>`)
--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,22 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/webAuth1'
+      filename: './data/webAuth1.db3',
     },
     pool: {
       afterCreate: (conn, done) => {
         conn.run('PRAGMA foreign_keys = ON', done);
-      }
+      },
     },
     migrations: {
-      directory: './data/migrations'
+      directory: './data/migrations',
     },
     seeds: {
-      directory: './data/seeds'
-    }
-  }
+      directory: './data/seeds',
+    },
+  },
 };
